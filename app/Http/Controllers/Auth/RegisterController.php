9360be3258fb2399d2919c8e1416fbbf27a796d1
Amandea96 <36308944+Amandea96@users.noreply.github.com>
2018-12-27 08:04:24 +0100

    Add files via upload

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/homePage';
 
     /**
      * Create a new controller instance.
@@ -36,10 +36,17 @@ class RegisterController extends Controller
      * @return void
      */
     public function __construct()
-    {
+    {	//if(!($this->middleware('guest')))
+		//{$redirectTo = '/register';}
         $this->middleware('guest');
     }
 
+	
+	
+	 protected function edit(array $data){
+		$redirectTo = '/register'; 
+		 
+	 }
     /**
      * Get a validator for an incoming registration request.
      *
@@ -49,9 +56,23 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'name' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+			'gender' => 'required|string|max:255',
+			'prefM' => 'required|string|max:255',
+			'age' => 'required|string|max:255',
+			'complex' => 'nullable|string|max:255',
+			'ecolor' => 'nullable|string|max:255',
+			'hcolor' => 'nullable|string|max:255',
+			'city' => 'required|string|max:255',
+			'prefW' => 'required|string|max:255',
+			'nation' => 'required|string|max:255',
+			'role' => 'nullable|string|max:255',
+			'height' => 'nullable|integer|between:110,230',
+			'image' => 'required|max:10000',
+			'description' => 'nullable|max:10000',
+			
         ]);
     }
 
@@ -62,11 +83,27 @@ class RegisterController extends Controller
      * @return \App\User
      */
     protected function create(array $data)
-    {
+    {	
+	
+	
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+			'gender' => $data['gender'],
+			'prefM' => $data['prefM'],
+			'age' => $data['age'],
+			'complex' => $data['complex'],
+			'ecolor' => $data['ecolor'],
+			'hcolor' => $data['hcolor'],
+			'city' => $data['city'],
+			'prefW' => $data['prefW'],
+			'nation' => $data['nation'],
             'password' => Hash::make($data['password']),
+			//'role' => $data['role'],
+			'height' => $data['height'],
+			'image' => $data['image'],
+	'description' => $data['description'],
+			
         ]);
     }
 }
